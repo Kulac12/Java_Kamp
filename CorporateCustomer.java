b5de4e87a38f92ccd5d2b6632204805a9fb80238
Kulac12 <71596094+Kulac12@users.noreply.github.com>
2021-05-14 23:07:45 +0300

    Delete CorporateCustomer.java

--- CorporateCustomer.java
+++
@@ -1,9 +0,0 @@
-package inheritance_miras;
-//kurumsal müþteri
-//bunlar özellik tutan sýnýflar
-public class CorporateCustomer extends Customer{
-	
-	 String companyName;
-	 String taxtNumber;
-	 
-}
